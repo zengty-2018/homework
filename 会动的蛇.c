c99d8db7dabeb71948e5753c8f993cc6ac2b667c
zengty-2018 <1258632312@qq.com>
2018-12-05 23:26:42 +0800

    会动的蛇

---
+++ 会动的蛇.c
@@ -0,0 +1,173 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+#define   WALL_LENTH   12
+#define   WALL         '*'
+#define   SNAKE_BODY   'X'
+#define   SNAKE_HEAD   'H'
+#define   AREA         ' '
+
+char map[12][12];
+int flag = 1;
+int snake_len = 5;
+int position[4][2];
+int head[2];
+int direction;
+
+void before_game();
+void play_game();
+void game_over();
+void starting_interface();
+void map_making();
+void snake_making();
+void output();
+void getDirection();
+void snakeMove();
+void isGameOver();
+int main(){
+	before_game();
+	play_game();
+	game_over();
+	
+	return 0;
+}
+
+void before_game(){
+	starting_interface();
+	map_making();
+	snake_making();
+}
+	
+void play_game(){
+	output();
+	
+	while(1){
+		getDirection();
+		snakeMove();
+		isGameOver();
+		
+		if(flag == 0)
+			break; 
+		
+		output();
+	}
+}
+		
+void game_over(){
+	printf("\n\n\n\n");
+	printf("                                           游戏结束\n------------------------------------------------------------------------------------------------------------\n");
+}
+
+void starting_interface(){
+	char c;
+	printf("                                            贪吃蛇            \n\n\n\n\n\n");
+	printf("                                        使用WASD控制方向       \n"); 
+	printf("\n");
+	printf("                                         按回车键开始\n------------------------------------------------------------------------------------------------------------\n");
+	if((c = getchar()) != EOF)
+		system("CLS"); 
+}
+
+void map_making(){
+	int i; 
+	for(i = 0; i < WALL_LENTH; i++){
+		map[0][i] = WALL;
+		map[WALL_LENTH-1][i] = WALL;
+		map[i][0] = WALL;
+		map[i][WALL_LENTH-1] = WALL;
+	}
+}
+
+void snake_making(){
+	int i,x=0;
+	for(i = 1; i < snake_len; i++){
+		map[1][i] = SNAKE_BODY;
+		position[x][0] = 1;
+		position[x][1] = i;
+		x++;
+	}
+	map[1][i] = SNAKE_HEAD;
+	head[0] = 1;
+	head[1] = i;
+}
+
+void output(){
+	int i,j;
+		for(i = 0; i < WALL_LENTH; i++){
+			for(j = 0; j < WALL_LENTH; j++){
+				printf("%c",map[i][j]);
+			}
+		printf("\n");
+	}
+}
+
+void getDirection(){
+	direction = getch();
+}
+
+void snakeMove(){
+	int i;
+	
+	switch (direction) {
+		case 97 : {map[position[0][0]][position[0][1]] = AREA;
+					for(i = 0; i < snake_len-2; i++){
+						position[i][0] = position[i+1][0];
+						position[i][1] = position[i+1][1];
+					}
+					position[i][0] = head[0];
+					position[i][1] = head[1];
+					head[1]--;
+				};
+				break;
+		case 119  : {map[position[0][0]][position[0][1]] = AREA;
+					for(i = 0; i < snake_len-2; i++){
+						position[i][0] = position[i+1][0];
+						position[i][1] = position[i+1][1];
+					}
+					position[i][0] = head[0];
+					position[i][1] = head[1];
+					head[0]--;
+				};
+				break;
+		case 100 : {map[position[0][0]][position[0][1]] = AREA;
+					for(i = 0; i < snake_len-2; i++){
+						position[i][0] = position[i+1][0];
+						position[i][1] = position[i+1][1];
+					}
+					position[i][0] = head[0];
+					position[i][1] = head[1];
+					head[1]++;
+				};
+				break;
+		case 115 : {map[position[0][0]][position[0][1]] = AREA;
+					for(i = 0; i < snake_len-2; i++){
+						position[i][0] = position[i+1][0];
+						position[i][1] = position[i+1][1];
+					}
+					position[i][0] = head[0];
+					position[i][1] = head[1];
+					head[0]++;
+				};
+				break;
+	}
+	
+	for(i = 0; i < snake_len-1; i++){
+		map[position[i][0]][position[i][1]] = SNAKE_BODY;
+	}
+	map[head[0]][head[1]] = SNAKE_HEAD;
+}
+
+void isGameOver(){
+	int i;
+	
+	if(head[0] == 0 || head[0] == 11)
+		flag = 0;
+	if(head[1] == 0 || head[1] == 11)
+		flag = 0;
+	
+	for(i = 0; i < snake_len-1; i++){
+		if(position[i][0] == head[0] && position[i][1] == head[1])
+			flag = 0;
+	}
+}
